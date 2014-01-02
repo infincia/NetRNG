a218fa1a4c1029a94ea970fde71873f99bfedd74
Stephen Oliver <steve@infincia.com>
2014-01-01 23:01:06 -0500

    add pip upgrade script

---
+++ upgrade-pip.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+ 
+import os
+import pip
+ 
+dists = []
+for dist in pip.get_installed_distributions():
+    dists.append(dist.project_name)
+ 
+for dist_name in sorted(dists, key=lambda s: s.lower()):
+    cmd = "pip install {0} -U".format(dist_name)
+    print '#', cmd
+    os.system(cmd)
+
