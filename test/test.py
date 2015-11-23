6ccd7c71e56c6a05a7099359756d5fed45deb204
Stephen Oliver <steve@infincia.com>
2015-11-22 23:53:59 -0500

    Add real code to the test script, will be migrated to py.test soon

--- test/test.py
+++ test/test.py
@@ -6,6 +6,22 @@ import sys
 
 import netrng.core
 
+def test_server():
+    server = netrng.core.Server(listen_address='127.0.0.1',
+                          port=8989,
+                          max_clients=2,
+                          sample_size_bytes=2048,
+                          hwrng_device='/dev/zero',
+                          use_zeroconf=True)
+
+    server.start()
+    
+def test_client():
+    client = netrng.core.Client(server_address='127.0.0.1', port=8989, use_zeroconf=True)
+    client.start()
+
 if __name__ == '__main__':
+    test_server()
+    test_client()
     sys.exit(0)
     
